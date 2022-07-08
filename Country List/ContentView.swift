583961684bc089b4d3cd00256fe0a9f80a5690b1
Mattia La Spina <mattia.laspina@atos.net>
2022-07-08 07:59:52 +0200

    Initial Commit

---
+++ Country List/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Country List
+//
+//  Created by Mattia La Spina on 8/7/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Country List/Country_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Country_ListApp.swift
+//  Country List
+//
+//  Created by Mattia La Spina on 8/7/22.
+//
+
+import SwiftUI
+
+@main
+struct Country_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
